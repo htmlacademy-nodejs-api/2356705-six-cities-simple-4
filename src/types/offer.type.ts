c371eaad8c464d95e99c35727930559d8988baf3
ekvek <kea@aobd.dev>
2023-05-04 20:49:30 +0300

    пункты 2,3 задания

---
+++ src/types/offer.type.ts
@@ -0,0 +1,23 @@
+import { Comfort } from './comfort.enum.js';
+import { OfferType } from './offer-type.enum.js';
+import { CityEnum } from './city.enum.js';
+import { User } from './user.type.js';
+import { Coordinates } from './coordinates.type.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+  postDate: Date;
+  city: CityEnum;
+  previewImage: string;
+  photos: string[];
+  premiumFlag: boolean;
+  rating: number;
+  type: OfferType;
+  rooms: number;
+  guests: number;
+  price: number;
+  comfort: Comfort[];
+  user: User;
+  coordinates: Coordinates;
+}
