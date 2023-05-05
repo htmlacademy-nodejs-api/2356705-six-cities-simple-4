9a9bee2756a7cf6cd43e7b244cdf605c697d45bc
ekvek <kea@aobd.dev>
2023-05-05 23:15:25 +0300

    Добавлена раскраска информации вывода в консоль

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -1,4 +1,7 @@
 import { CliCommandInterface } from './cli-command.interface.js';
+import { Chalk } from 'chalk';
+
+const customChalk = new Chalk({level: 1});
 
 export default class HelpCommand implements CliCommandInterface {
   public readonly name = '--help';
@@ -6,12 +9,12 @@ export default class HelpCommand implements CliCommandInterface {
   public async execute(): Promise<void> {
     console.log(`
         Программа для подготовки данных для REST API сервера.
-        Пример: cli.js --<command> [--arguments]
+        Пример: cli.js --<${customChalk.blue('command')}> [${customChalk.green('--arguments')}]
         Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+        ${customChalk.green('--version')}:                   ${customChalk.magenta('# выводит номер версии')}
+        ${customChalk.green('--help')}:                      ${customChalk.magenta('# печатает этот текст')}
+        ${customChalk.green('--import')} <path>:             ${customChalk.magenta('# импортирует данные из TSV')}
+        ${customChalk.green('--generate')} <n> <path> <url>  ${customChalk.magenta('# генерирует произвольное количество тестовых данных')}
         `);
   }
 }
